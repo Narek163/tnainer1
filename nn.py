937bf8e1caabb22d3e5d0070686b60f2f564c929
narek163 <narek.na2004@mail.ru>
2022-04-06 19:46:24 +0400

    tnainer

--- nn.py
+++ nn.py
@@ -1,2 +1,13 @@
-for i in range(1,10):
-    print(i)
+#  f = open('txt2.txt','rt')
+# res = f.read() 
+# list1 = []  
+# for i in res:         
+#     if i.isdigit():
+#         list1.append(i)
+# print(list1)
+
+#  res = f.read()
+# res = res.split('\n')        
+# res = sorted(res, key=len)
+# print(res[-1]
+
