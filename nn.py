68a70aa93107e387b434756f26e00f5b2b596279
narek163 <narek.na2004@mail.ru>
2022-04-20 20:14:38 +0400

    t

--- nn.py
+++ nn.py
@@ -1,13 +1,56 @@
-#  f = open('txt2.txt','rt')
-# res = f.read() 
-# list1 = []  
-# for i in res:         
-#     if i.isdigit():
+
+
+# 3
+
+# '''porceci chstacvec'''
+
+
+
+
+# 4 
+# def func(list1,res):
+#     list1.sort(key=len)
+#     print(len(list1[-1]) + len(list1[0]))
+
+# func(['anymore','rven','me','communicate'],0)
+
+
+
+# 5 
+
+# def index(list1,n):
+#     for i in range(0,len(list1)):
+#         if n == list1[i]:
+#             print(i)
+# index([21,-9,15,2116,-71,33],-71
+
+
+# 6 
+
+# def dict(dict1,list1,list2,n):
+#     for i in range(1,n +1):
 #         list1.append(i)
-# print(list1)
+#         list2.append(i**2)
+#         for x,y in zip(list1,list2):
+#             dict1.setdefault(x,y)
+#     print(dict1)
+# dict({},[],[],5)
 
-#  res = f.read()
-# res = res.split('\n')        
-# res = sorted(res, key=len)
-# print(res[-1]
 
+# 7 
+
+# '''ishqan porceci chkaroxaca'''
+        
+
+
+
+# 8 
+
+# def fib(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fib(n - 1) + fib(n - 2)
+# print(fib(6))
